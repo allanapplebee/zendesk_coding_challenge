38c24d0217890cb07f6b5ea605b36848cb06b94b
Allan Applebee <allan.applebee@gmail.com>
2019-06-10 15:14:04 +1000

    show individual ticket page working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const axios = require('axios');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //server consts
 const port = process.env.PORT || 3000;
@@ -12,6 +13,10 @@ const app = express();
 
 //set ejs as templating engine
 app.set('view engine', 'ejs');
+
+//set up body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 //Routes
 app.get('/', (req, res) => {
@@ -29,10 +34,27 @@ app.get('/listall', async (req, res) => {
                 per_page: 25
             }
         });
-        console.log(ticketList.data.tickets)
+        console.log(ticketList.data.tickets[1])
         res.render('listAllTickets', {ticketList: ticketList});
     } catch (err) {
         console.log(err);
+    }
+});
+
+app.post('/showticket', async (req, res) => {
+    try {
+        let id = req.body.id
+        let url = `https://allanapplebee.zendesk.com/api/v2/tickets/${id}.json`;
+        const ticket = await axios.get(url, {
+            auth: {
+                username: process.env.EMAIL,
+                password: process.env.PASSWORD
+            }
+        })
+        console.log(id)
+        res.render('showTicket', {ticket: ticket})
+    } catch(err){
+        console.log(err)
     }
 })
 
