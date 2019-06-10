41b83bc5f811862982c18e6eb357ad2672cca1e5
Allan Applebee <allan.applebee@gmail.com>
2019-06-10 10:08:29 +1000

    basic server set up and working. Merging to develop so can work on front end

---
+++ index.js
@@ -0,0 +1,43 @@
+// Require packages
+require('dotenv').config();
+const axios = require('axios');
+const express = require('express');
+
+//server consts
+const port = process.env.PORT || 3000;
+const IP = process.env.IP;
+
+//initialise express
+const app = express();
+
+//set ejs as templating engine
+app.set('view engine', 'ejs');
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('hello')
+});
+
+app.get('/listall', async (req, res) => {
+    try {
+        const ticketList = await axios.get(process.env.URL, {
+            auth: {
+                username: process.env.EMAIL,
+                password: process.env.PASSWORD
+            },
+            params: {
+                per_page: 25
+            }
+        });
+        console.log(ticketList.data.tickets)
+        res.render('listAllTickets', {ticketList: ticketList});
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+// listAllTickets();
+
+app.listen(port, IP, () => {
+    console.log("Server Has Started...");
+ });
